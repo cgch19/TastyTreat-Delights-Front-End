17b9a47252a07b5b07912ce3e3ae76f7de85ea5f
cgch8 <carloschie15@gmail.com>
2024-05-30 18:04:57 -0700

    updated the homepage

--- tastytreat-delight-front-end/src/pages/Homepage.js
+++ tastytreat-delight-front-end/src/pages/Homepage.js
@@ -3,32 +3,41 @@ import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
 
 function HomePage() {
   return (
-    <Box textAlign="center" p={4}>
-      <header className='bg to-blue-400'>
-        <Typography variant="h4" gutterBottom>
-          Welcome to Tasty Treat Delights
-        </Typography>
-      </header>
-      <section>
-        <Typography variant="body1" gutterBottom>
-          Explore our delicious treats and indulge your cravings!
-        </Typography>
-
-        <Link to="/product-catalog">
-          <Button variant="contained" color="primary">
-            View Treats
-          </Button>
-        </Link>
-      </section>
-      <footer>
-        <Typography variant="body2" fontStyle="italic" mt={4}>
+    <Container 
+      sx={{ 
+        minHeight: '100vh', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        justifyContent: 'space-between' 
+      }}
+    >
+      <Box textAlign="center" p={4}>
+        <header>
+          <Typography variant="h4" gutterBottom>
+            Welcome to Tasty Treat Delights
+          </Typography>
+        </header>
+        <section style={{ marginTop: '20px' }}>
+          <Typography variant="body1" gutterBottom>
+            Explore our delicious treats and indulge your cravings!
+          </Typography>
+          <Link to="/product-catalog" style={{ textDecoration: 'none' }}>
+            <Button variant="contained" color="primary" size="large" sx={{ mt: 2 }}>
+              View Treats
+            </Button>
+          </Link>
+        </section>
+      </Box>
+      <Box component="footer" textAlign="center" py={2}>
+        <Typography variant="body2" fontStyle="italic">
           © 2024 Tasty Treat Delights. All rights reserved.
         </Typography>
-      </footer>
-    </Box>
+      </Box>
+    </Container>
   );
 }
 
